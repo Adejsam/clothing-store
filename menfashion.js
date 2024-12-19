c9240dfba80d32f24e539fb305bceddf927df539
Adejumobi Ayotomiwa Samuel <adejusamuel@gmail.com>
2024-12-19 12:45:25 +0100

    first commit

---
+++ menfashion.js
@@ -0,0 +1,11 @@
+function favHeart(image) {
+    // Extract the file name part of the src
+    var currentSrc = image.src.split("/").pop(); // Get only the filename
+  
+    // Toggle the src based on the current filename
+    if (currentSrc === "heart.svg") {
+        image.src = "./public/icon/fill%20heart.svg"; // Ensure no spaces in filenames
+    } else {
+        image.src = "./public/icon/heart.svg";
+    }
+  }
